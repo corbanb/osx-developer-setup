c282d0a96004b432d95bf49dbdb8f58144bfe064
corban baxter <corbanb@gmail.com>
2023-04-28 13:22:46 -0600

    Update macos-nodejs-dev.sh
    cleanup

--- setups/macos-nodejs-dev.sh
+++ setups/macos-nodejs-dev.sh
@@ -3,55 +3,53 @@
 
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 # Install OhMyZSH
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Install OSX Developer Tools
 xcode-select --install
 
 
-
 # Install Apps
-brew install hyper visual-studio-code google-chrome slack sourcetree docker arc spotify zoom github raycast
-
+brew install google-chrome arc 
+brew install discord slack zoom 
+brew install notion raycast
+brew install visual-studio-code postman sourcetree github
+brew install spotify 1password
+brew install docker hyper iterm2
 
 # Install VSCode Theming
-code --install-extension GitHub.github-vscode-theme # Github theme
+code --install-extension GitHub.github-vscode-theme
 code --install-extension vscode-icons-team.vscode-icons
 
 # JS Developer Extensions
-code --install-extension christian-kohler.npm-intellisense # npm auto complete
+code --install-extension christian-kohler.npm-intellisense 
 code --install-extension dbaeumer.vscode-eslint
 
-
 # Developer Tools
-code --install-extension GitHub.copilot # GitHub Copilot 
-code --install-extension GitHub.vscode-pull-request-github # Github Issues and PRs
-code --install-extension streetsidesoftware.code-spell-checker # spell checking
-code --install-extension oderwat.indent-rainbow # indent rainbows
-code --install-extension ms-azuretools.vscode-docker # docker management
-code --install-extension github.vscode-github-actions # github actions
-code --install-extension eamodio.gitlens # gitlens
-code --install-extension rangav.vscode-thunder-client # REST API Client
+code --install-extension GitHub.copilot 
+code --install-extension GitHub.vscode-pull-request-github 
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension oderwat.indent-rainbow 
+code --install-extension ms-azuretools.vscode-docker 
+code --install-extension github.vscode-github-actions 
+code --install-extension eamodio.gitlens 
+code --install-extension rangav.vscode-thunder-client 
 
 # Lanugague & File Support
-code --install-extension golang.go # golang support
-code --install-extension mikestead.dotenv # dotenv files
-code --install-extension mechatroner.rainbow-csv #rainbow CSV files
-code --install-extension bierner.markdown-preview-github-styles # markdown previewer
-
+code --install-extension golang.go 
+code --install-extension mikestead.dotenv 
+code --install-extension mechatroner.rainbow-csv 
+code --install-extension bierner.markdown-preview-github-styles 
 
 # Install CLI Tools
 brew tap heroku/brew
 brew install node nvm zsh git gh awscli pnpm heroku
 
-# Install PHP Tools
-sudo apachectl stop
-sudo launchctl unload -w /System/Library/LaunchDaemons/org.apache.httpd.plist 2>/dev/null
-
-
 # TODO: Add in PHP setup
+# Install PHP Tools
+# sudo apachectl stop
+# sudo launchctl unload -w /System/Library/LaunchDaemons/org.apache.httpd.plist 2>/dev/null
+
 # brew tap homebrew/php
 # brew tap homebrew/dupes
 
@@ -64,15 +62,16 @@ sudo launchctl unload -w /System/Library/LaunchDaemons/org.apache.httpd.plist 2>
 # sudo brew services start httpd
 
 
-# Open Hyper to allow plugins
-open /Applications/Hyper.app
+## TODO: break out the details for terminal setup
+# # Open Hyper to allow plugins
+# open /Applications/Hyper.app
+
+# # add hyper plugins
+# hpm install hyperlayout hyperterm-working-directory
+
 
 # Global NPM Modules
 npm install -g yarn
-
-# add hyper plugins
-hpm install hyperlayout hyperterm-working-directory
-
 
 
 # Setup NVM
@@ -81,13 +80,8 @@ nvm install 14 --reinstall-packages-from=system
 nvm install 16 --reinstall-packages-from=system
 nvm install 18 --reinstall-packages-from=system
 
-
-
 # Setup Sites and Development
-mkdir ~/Sites && ~/Development
-
-
-
+mkdir ~/Sites && mkdir ~/Development
 
 # Setup Profile
 touch ~/.oh-my-zsh/custom/profile.zsh
@@ -125,7 +119,6 @@ read email
 
 echo What is your website?
 read website
-
 
 touch ~/.npmrc
 cat <<EOF >~/.npmrc
