fe6596599fb63a277af840db84414a1a149dc7e4
corban baxter <corbanb@gmail.com>
2023-05-04 20:32:19 -0600

    adding inital dotfiles

---
+++ dotfiles/profile.zsh
@@ -0,0 +1,35 @@
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+export PATH="/Users/$HOME/.local/bin:$PATH"
+
+autoload -U add-zsh-hook
+
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
+
+alias profile="code ~/.oh-my-zsh/custom/profile.zsh"
+alias sprofile="source ~/.oh-my-zsh/custom/profile.zsh"
+alias g="git"
+alias ls="ls -alG"
+alias dev="cd ~/Development"
+alias myip="curl -w '\n' icanhazip.com"
+alias dotfiles="code $HOME/Development/osx-developer-setup"
